7bc9e539ccf4111459978bddf52851a5dcdb5b36
Jake Angerman <jake@redislabs.com>
2016-10-06 07:34:05 -0500

    catching all exceptions

--- doit.py
+++ doit.py
@@ -16,18 +16,26 @@ signal.signal(signal.SIGINT, signal_handler)
 sentinel = Sentinel([('localhost', 26379), ('192.168.1.112', 26379)], socket_timeout=0.1)
 master = sentinel.master_for('mypod', socket_timeout=0.1)
 slave = sentinel.slave_for('mypod', socket_timeout=0.1)
-#pid = os.getpid()
+#pid = os.getpid() #print("%d:P " % pid, end="")
 #master.set("foo", 0)
 slave.set_response_callback('GET', int)
 
 while True:
-	master_tuple = sentinel.discover_master('mypod')
-	master_str = "%s:%d" % (master_tuple[0], master_tuple[1])
-	slave_list = sentinel.discover_slaves('mypod')
-	slave_str = "%s:%d" % (slave_list[0][0], slave_list[0][1])
-	write = master.incr("foo", 1)
-	read = slave.get("foo")
-	#print("%d:P " % pid, end="")
-	print(datetime.now().strftime('%d %b %H:%M:%S.%f')[:-3], end="")
-	print(" >>> wrote %d to master %s, read %d from slave %s" % (write, master_str, read, slave_str), flush=True)
+	try:
+		master_tuple = sentinel.discover_master('mypod')
+		master_str = "%s:%d" % (master_tuple[0], master_tuple[1])
+		slave_list = sentinel.discover_slaves('mypod')
+		if len(slave_list) > 0:
+			slave_str = "%s:%d" % (slave_list[0][0], slave_list[0][1])
+		else:
+			slave_str = master_str
+		write = master.incr("foo", 1)
+		read = slave.get("foo")
+		print(datetime.now().strftime('%d %b %H:%M:%S.%f')[:-3], end="")
+		print(" >>> wrote %d to master %s, read %d from slave %s" % (write, master_str, read, slave_str), flush=True)
+	except:
+		e = sys.exc_info()[0]
+		print(">>>>>>>>>>>>>>>>>> ERROR %s" % e, flush=True)
+		#sys.exit(0)
+
 	time.sleep(1)
